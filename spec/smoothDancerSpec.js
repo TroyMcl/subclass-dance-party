56b4c382f35a24a91c66933853f5cd5c13596a9c
Emily Kwak <emilykwak1@gmail.com>
2019-06-26 20:21:47 -0700

    Complete two new dancers

---
+++ spec/smoothDancerSpec.js
@@ -0,0 +1,34 @@
+describe('smoothDancer', function() {
+
+  var smoothDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    smoothDancer = new makeSmoothDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(smoothDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it('should have a slide function that makes its node slide', function() {
+    sinon.spy(smoothDancer.$node, 'css');
+    smoothDancer.slide();
+    expect(smoothDancer.$node.css.called).to.be.true;
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(smoothDancer, 'step');
+      expect(smoothDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(smoothDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(smoothDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
